c7a8d453e2252a8d993cad7641684935b360a6ec
SooYeonKim <lovesu5632@gmail.com>
2022-01-19 18:52:48 +0900

    ADD : 2022-01-19

---
+++ 2022_January/2022_01_13_assignment/SY_도둑질.java
@@ -0,0 +1,22 @@
+package study0120;
+
+public class SY_도둑질 {
+    public int solution(int[] money) {
+        int[] dp = new int[money.length];
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
+        for (int i = 2; i < money.length - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+        dp[money.length - 1] = dp[money.length - 2];
+        
+        int[] dp2 = new int[money.length];
+        dp2[1] = money[1];
+        for (int i = 2; i < money.length; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+        }
+        
+        int answer = Math.max(dp[money.length - 1], dp2[money.length - 1]);
+        return answer;
+    }
+}
