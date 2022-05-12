38bceabdc08d43f0a2d586b82ce8a9ecc08a81fb
sny3238 <sny3238@gmail.com>
2022-05-12 22:25:41 +0900

    ADD : 2022-05-12

---
+++ 2022_April/2022_04_28_assignment/NY_올바른_괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack  = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char c= s.charAt(i);
+            if(c == '('){
+                stack.push('(');
+            }else {
+                if(!stack.isEmpty() && stack.peek() == '('){
+                   stack.pop();
+                }else
+                    return false;
+                    
+            }
+        }
+        
+        if(stack.isEmpty())
+        return true;
+        else
+            return false;
+    }
+}
