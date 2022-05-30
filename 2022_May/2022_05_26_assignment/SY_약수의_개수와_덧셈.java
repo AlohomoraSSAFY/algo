c8490964bfec65af8d5ccf4b71acc450da1be508
SooYeonKim <lovesu5632@gmail.com>
2022-05-30 22:31:14 +0900

    ADD : 2022-05-30

---
+++ 2022_May/2022_05_26_assignment/SY_약수의_개수와_덧셈.java
@@ -0,0 +1,25 @@
+package study0602;
+
+public class SY_약수의_개수와_덧셈 {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                if (j * j == i) {
+                    cnt++;
+                } else if (i % j == 0) {
+                    cnt += 2;
+                }
+            }
+            
+            if (cnt % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+}
