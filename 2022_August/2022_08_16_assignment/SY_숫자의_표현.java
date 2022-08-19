a8a8dff5afd42d604a574d0b1f1b37d730721ed8
SooYeonKim <lovesu5632@gmail.com>
2022-08-19 11:33:45 +0900

    ADD : 2022-08-19

---
+++ 2022_August/2022_08_16_assignment/SY_숫자의_표현.java
@@ -0,0 +1,20 @@
+package study0823;
+
+public class SY_숫자의_표현 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
